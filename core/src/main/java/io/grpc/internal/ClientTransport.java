371fdd647888a48d776baabe92e9ec148d9103d7
raihan <54474184+ryihan@users.noreply.github.com>
2021-11-06 22:30:40 +0600

    Update ClientTransport.java

--- core/src/main/java/io/grpc/internal/ClientTransport.java
+++ core/src/main/java/io/grpc/internal/ClientTransport.java
@@ -28,3 +28,23 @@ import javax.annotation.concurrent.ThreadSafe;
 /**
  * The client-side transport typically encapsulating a single connection to a remote
  * server. However, streams created before the client has discovered any server address may
+ * eventually be issued on different connections.  All methods on the transport and its callbacks
+ * are expected to execute quickly.
+ */
+@ThreadSafe
+public interface ClientTransport extends InternalInstrumented<SocketStats> {
+
+  /**
+   * Creates a new stream for sending messages to a remote end-point.
+   *
+   * <p>This method returns immediately and does not wait for any validation of the request. If
+   * creation fails for any reason, {@link ClientStreamListener#closed} will be called to provide
+   * the error information. Any sent messages for this stream will be buffered until creation has
+   * completed (either successfully or unsuccessfully).
+   *
+   * <p>This method is called under the {@link io.grpc.Context} of the {@link io.grpc.ClientCall}.
+   *
+   * @param method the descriptor of the remote method to be called for this stream.
+   * @param headers to send at the beginning of the call
+   * @param callOptions runtime options of the call
+   * @param tracers a non-empty array of tracers. The last element in it is reserved to be set by
