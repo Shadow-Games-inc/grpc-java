1d986a4644a6531070a062087202b2b7e27370eb
raihan <54474184+ryihan@users.noreply.github.com>
2021-11-06 22:30:22 +0600

    Update ClientStream.java

--- core/src/main/java/io/grpc/internal/ClientStream.java
+++ core/src/main/java/io/grpc/internal/ClientStream.java
@@ -101,3 +101,8 @@ public interface ClientStream extends Stream {
 
   /**
    * Append information that will be included in the locally generated DEADLINE_EXCEEDED errors to
+   * the given {@link InsightBuilder}, in order to tell the user about the state of the stream so
+   * that they can better diagnose the cause of the error.
+   */
+  void appendTimeoutInsight(InsightBuilder insight);
+}
