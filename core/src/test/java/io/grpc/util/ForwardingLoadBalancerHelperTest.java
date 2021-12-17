9448bda20bd6321d5e8e4c8a08ad78cbdf05a9eb
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-17 12:46:44 +0600

    Update ForwardingLoadBalancerHelperTest.java

--- core/src/test/java/io/grpc/util/ForwardingLoadBalancerHelperTest.java
+++ core/src/test/java/io/grpc/util/ForwardingLoadBalancerHelperTest.java
@@ -1,3 +1,4 @@
+/*
  * Copyright 2018 The gRPC Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -27,3 +28,38 @@ import java.util.Collections;
 import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Unit tests for {@link ForwardingLoadBalancerHelper}. */
+@RunWith(JUnit4.class)
+public class ForwardingLoadBalancerHelperTest {
+  private final LoadBalancer.Helper mockDelegate = mock(LoadBalancer.Helper.class);
+
+  private final class TestHelper extends ForwardingLoadBalancerHelper {
+    @Override
+    protected LoadBalancer.Helper delegate() {
+      return mockDelegate;
+    }
+  }
+
+  @Test
+  public void allMethodsForwarded() throws Exception {
+    final SocketAddress mockAddr = mock(SocketAddress.class);
+    ForwardingTestUtil.testMethodsForwarded(
+        LoadBalancer.Helper.class,
+        mockDelegate,
+        new TestHelper(),
+        Collections.<Method>emptyList(),
+        new ForwardingTestUtil.ArgumentProvider() {
+          @Override
+          public Object get(Method method, int argPos, Class<?> clazz) {
+            if (clazz.equals(EquivalentAddressGroup.class)) {
+              return new EquivalentAddressGroup(Arrays.asList(mockAddr));
+            } else if (clazz.equals(List.class)) {
+              return Collections.emptyList();
+            }
+            return null;
+          }
+        });
+  }
+}
