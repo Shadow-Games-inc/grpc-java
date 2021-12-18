68d73e4bcbf1f4bcdef79d253b12a0f057cc7468
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-18 13:41:17 +0600

    Update HelloWorldAltsServer.java

--- examples/example-alts/src/main/java/io/grpc/examples/alts/HelloWorldAltsServer.java
+++ examples/example-alts/src/main/java/io/grpc/examples/alts/HelloWorldAltsServer.java
@@ -1,3 +1,4 @@
+/*
  * Copyright 2018 The gRPC Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -24,3 +25,75 @@ import io.grpc.stub.StreamObserver;
 import java.io.IOException;
 import java.util.concurrent.Executors;
 import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * An example gRPC server that uses ALTS. Shows how to do a Unary RPC. This example can only be run
+ * on Google Cloud Platform.
+ */
+public final class HelloWorldAltsServer extends GreeterImplBase {
+  private static final Logger logger = Logger.getLogger(HelloWorldAltsServer.class.getName());
+  private Server server;
+  private int port = 10001;
+
+  public static void main(String[] args) throws IOException, InterruptedException {
+    new HelloWorldAltsServer().start(args);
+  }
+
+  private void parseArgs(String[] args) {
+    boolean usage = false;
+    for (String arg : args) {
+      if (!arg.startsWith("--")) {
+        System.err.println("All arguments must start with '--': " + arg);
+        usage = true;
+        break;
+      }
+      String[] parts = arg.substring(2).split("=", 2);
+      String key = parts[0];
+      if ("help".equals(key)) {
+        usage = true;
+        break;
+      }
+      if (parts.length != 2) {
+        System.err.println("All arguments must be of the form --arg=value");
+        usage = true;
+        break;
+      }
+      String value = parts[1];
+      if ("port".equals(key)) {
+        port = Integer.parseInt(value);
+      } else {
+        System.err.println("Unknown argument: " + key);
+        usage = true;
+        break;
+      }
+    }
+    if (usage) {
+      HelloWorldAltsServer s = new HelloWorldAltsServer();
+      System.out.println(
+          "Usage: [ARGS...]"
+              + "\n"
+              + "\n  --port=PORT           Server port to bind to. Default "
+              + s.port);
+      System.exit(1);
+    }
+  }
+
+  private void start(String[] args) throws IOException, InterruptedException {
+    parseArgs(args);
+    server =
+        AltsServerBuilder.forPort(port)
+            .addService(this)
+            .executor(Executors.newFixedThreadPool(1))
+            .build();
+    server.start();
+    logger.log(Level.INFO, "Started on {0}", port);
+    server.awaitTermination();
+  }
+
+  @Override
+  public void sayHello(HelloRequest request, StreamObserver<HelloReply> observer) {
+    observer.onNext(HelloReply.newBuilder().setMessage("Hello, " + request.getName()).build());
+    observer.onCompleted();
+  }
+}
