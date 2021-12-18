60646e2b6d1c1bddc794046ac242899ba412f93b
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-18 13:43:16 +0600

    Update JwtCredential.java

--- examples/example-jwt-auth/src/main/java/io/grpc/examples/jwtauth/JwtCredential.java
+++ examples/example-jwt-auth/src/main/java/io/grpc/examples/jwtauth/JwtCredential.java
@@ -31,3 +31,38 @@ public class JwtCredential extends CallCredentials {
 
   private final String subject;
 
+  JwtCredential(String subject) {
+    this.subject = subject;
+  }
+
+  @Override
+  public void applyRequestMetadata(final RequestInfo requestInfo, final Executor executor,
+      final MetadataApplier metadataApplier) {
+    // Make a JWT compact serialized string.
+    // This example omits setting the expiration, but a real application should do it.
+    final String jwt =
+        Jwts.builder()
+            .setSubject(subject)
+            .signWith(SignatureAlgorithm.HS256, Constant.JWT_SIGNING_KEY)
+            .compact();
+
+    executor.execute(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          Metadata headers = new Metadata();
+          headers.put(Constant.AUTHORIZATION_METADATA_KEY,
+              String.format("%s %s", Constant.BEARER_TYPE, jwt));
+          metadataApplier.apply(headers);
+        } catch (Throwable e) {
+          metadataApplier.fail(Status.UNAUTHENTICATED.withCause(e));
+        }
+      }
+    });
+  }
+
+  @Override
+  public void thisUsesUnstableApi() {
+    // noop
+  }
+}
