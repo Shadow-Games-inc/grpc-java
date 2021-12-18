99ef3525671261a192146e2b5b478eff0483dee3
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-18 13:42:19 +0600

    Create HostnameServer.java

---
+++ examples/example-hostname/src/main/java/io/grpc/examples/hostname/HostnameServer.java
@@ -0,0 +1,20 @@
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.examples.hostname;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.health.v1.HealthCheckResponse.ServingStatus;
+import io.grpc.protobuf.services.ProtoReflectionService;
+import io.grpc.services.HealthStatusManager;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * A server that hosts HostnameGreeter, plus infrastructure services like health and reflection.
