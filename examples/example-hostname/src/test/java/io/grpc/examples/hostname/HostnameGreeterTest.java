24db7a03cefa623a5661a93ab341bb395a3dd9a6
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-18 13:42:46 +0600

    Update HostnameGreeterTest.java

--- examples/example-hostname/src/test/java/io/grpc/examples/hostname/HostnameGreeterTest.java
+++ examples/example-hostname/src/test/java/io/grpc/examples/hostname/HostnameGreeterTest.java
@@ -1,3 +1,5 @@
+/*
+ * Copyright 2020 The gRPC Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,3 +24,47 @@ import io.grpc.examples.helloworld.HelloRequest;
 import io.grpc.examples.helloworld.HelloReply;
 import io.grpc.examples.hostname.HostnameGreeter;
 import io.grpc.inprocess.InProcessChannelBuilder;
+import io.grpc.inprocess.InProcessServerBuilder;
+import io.grpc.testing.GrpcCleanupRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Unit tests for {@link HostnameGreeter}.
+ *
+ * <p>This is very similar to HelloWorldServerTest, so see it for more descriptions.
+ */
+@RunWith(JUnit4.class)
+public class HostnameGreeterTest {
+  @Rule
+  public final GrpcCleanupRule grpcCleanup = new GrpcCleanupRule();
+
+  private GreeterGrpc.GreeterBlockingStub blockingStub =
+        GreeterGrpc.newBlockingStub(grpcCleanup.register(
+            InProcessChannelBuilder.forName("hostname").directExecutor().build()));
+
+  @Test
+  public void sayHello_fixedHostname() throws Exception {
+    grpcCleanup.register(
+        InProcessServerBuilder.forName("hostname")
+          .directExecutor().addService(new HostnameGreeter("me")).build().start());
+
+    HelloReply reply =
+        blockingStub.sayHello(HelloRequest.newBuilder().setName("you").build());
+    assertEquals("Hello you, from me", reply.getMessage());
+  }
+
+  @Test
+  public void sayHello_dynamicHostname() throws Exception {
+    grpcCleanup.register(
+        InProcessServerBuilder.forName("hostname")
+          .directExecutor().addService(new HostnameGreeter(null)).build().start());
+
+    // Just verifing the service doesn't crash
+    HelloReply reply =
+        blockingStub.sayHello(HelloRequest.newBuilder().setName("anonymous").build());
+    assertTrue(reply.getMessage(), reply.getMessage().startsWith("Hello anonymous, from "));
+  }
+}
