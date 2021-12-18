8ce71a348a11f005eb6af380b85ce796be3e2fbb
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-18 13:43:12 +0600

    Update Constant.java

--- examples/example-jwt-auth/src/main/java/io/grpc/examples/jwtauth/Constant.java
+++ examples/example-jwt-auth/src/main/java/io/grpc/examples/jwtauth/Constant.java
@@ -28,3 +28,10 @@ final class Constant {
     static final String JWT_SIGNING_KEY = "L8hHXsaQOUjk5rg7XPGv4eL36anlCrkMz8CJ0i/8E/0=";
     static final String BEARER_TYPE = "Bearer";
 
+    static final Metadata.Key<String> AUTHORIZATION_METADATA_KEY = Metadata.Key.of("Authorization", ASCII_STRING_MARSHALLER);
+    static final Context.Key<String> CLIENT_ID_CONTEXT_KEY = Context.key("clientId");
+
+    private Constant() {
+        throw new AssertionError();
+    }
+}
