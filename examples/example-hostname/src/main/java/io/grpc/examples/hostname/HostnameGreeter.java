9678f4e3abe4f72e3781a1f7b02027a2c9b7011e
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-18 13:42:16 +0600

    Update HostnameGreeter.java

--- examples/example-hostname/src/main/java/io/grpc/examples/hostname/HostnameGreeter.java
+++ examples/example-hostname/src/main/java/io/grpc/examples/hostname/HostnameGreeter.java
@@ -1,3 +1,5 @@
+/*
+ * Copyright 2020 The gRPC Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,3 +26,35 @@ import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/** Greeter implementation which replies identifying itself with its hostname. */
+public final class HostnameGreeter extends GreeterGrpc.GreeterImplBase {
+  private static final Logger logger = Logger.getLogger(HostnameGreeter.class.getName());
+
+  private final String serverName;
+
+  public HostnameGreeter(String serverName) {
+    if (serverName == null) {
+      serverName = determineHostname();
+    }
+    this.serverName = serverName;
+  }
+
+  @Override
+  public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
+    HelloReply reply = HelloReply.newBuilder()
+        .setMessage("Hello " + req.getName() + ", from " + serverName)
+        .build();
+    responseObserver.onNext(reply);
+    responseObserver.onCompleted();
+  }
+
+  private static String determineHostname() {
+    try {
+      return InetAddress.getLocalHost().getHostName();
+    } catch (IOException ex) {
+      logger.log(Level.INFO, "Failed to determine hostname. Will generate one", ex);
+    }
+    // Strange. Well, let's make an identifier for ourselves.
+    return "generated-" + new Random().nextInt();
+  }
+}
