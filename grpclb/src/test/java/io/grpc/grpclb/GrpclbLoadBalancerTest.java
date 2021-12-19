fb313d616667cf0c6eee81b6b5ca7b76ae938b1b
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-20 00:26:54 +0600

    Create GrpclbLoadBalancerTest.java

---
+++ grpclb/src/test/java/io/grpc/grpclb/GrpclbLoadBalancerTest.java
@@ -0,0 +1,13 @@
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.grpclb;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.truth.Truth.assertThat;
+import static io.grpc.ConnectivityState.CONNECTING;
+import static io.grpc.ConnectivityState.IDLE;
+import static io.grpc.ConnectivityState.READY;
+import static io.grpc.ConnectivityState.SHUTDOWN;
