dd0c5c4d0d59105c1ab2b8545a3a7f4a61f34df3
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-20 00:27:32 +0600

    Create XdsUpdateClientConfigureServiceGrpc.java

---
+++ interop-testing/src/generated/main/grpc/io/grpc/testing/integration/XdsUpdateClientConfigureServiceGrpc.java
@@ -0,0 +1,14 @@
+@javax.annotation.Generated(
+    value = "by gRPC proto compiler",
+    comments = "Source: grpc/testing/test.proto")
+@io.grpc.stub.annotations.GrpcGenerated
+public final class XdsUpdateClientConfigureServiceGrpc {
+
+  private XdsUpdateClientConfigureServiceGrpc() {}
+
+  public static final String SERVICE_NAME = "grpc.testing.XdsUpdateClientConfigureService";
+
+  // Static method descriptors that strictly reflect the proto.
+  private static volatile io.grpc.MethodDescriptor<io.grpc.testing.integration.Messages.ClientConfigureRequest,
+      io.grpc.testing.integration.Messages.ClientConfigureResponse> getConfigureMethod;
+
