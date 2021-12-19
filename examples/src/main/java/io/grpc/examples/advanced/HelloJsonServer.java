5b186b3218554229f62a96406f1a91be4c7a0776
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-20 00:19:08 +0600

    Create HelloJsonServer.java

---
+++ examples/src/main/java/io/grpc/examples/advanced/HelloJsonServer.java
@@ -0,0 +1,19 @@
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.examples.advanced;
+
+import static io.grpc.stub.ServerCalls.asyncUnaryCall;
+
+import io.grpc.BindableService;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.ServerServiceDefinition;
+import io.grpc.examples.helloworld.GreeterGrpc;
+import io.grpc.examples.helloworld.HelloReply;
+import io.grpc.examples.helloworld.HelloRequest;
