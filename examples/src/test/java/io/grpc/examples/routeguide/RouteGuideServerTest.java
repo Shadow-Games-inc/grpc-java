940ef34e51785b2cf5e9ad2182253ddfceef6d7d
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-20 00:23:52 +0600

    Create RouteGuideServerTest.java

---
+++ examples/src/test/java/io/grpc/examples/routeguide/RouteGuideServerTest.java
@@ -0,0 +1,15 @@
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.examples.routeguide;
+
+import static junit.framework.TestCase.fail;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.verify;
