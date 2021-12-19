c8551f0ffcd8182c05b90572e94f35253206e241
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-20 00:27:17 +0600

    Create ReconnectServiceGrpc.java

---
+++ interop-testing/src/generated/main/grpc/io/grpc/testing/integration/ReconnectServiceGrpc.java
@@ -0,0 +1,18 @@
+ * </pre>
+ */
+@javax.annotation.Generated(
+    value = "by gRPC proto compiler",
+    comments = "Source: grpc/testing/test.proto")
+@io.grpc.stub.annotations.GrpcGenerated
+public final class ReconnectServiceGrpc {
+
+  private ReconnectServiceGrpc() {}
+
+  public static final String SERVICE_NAME = "grpc.testing.ReconnectService";
+
+  // Static method descriptors that strictly reflect the proto.
+  private static volatile io.grpc.MethodDescriptor<io.grpc.testing.integration.EmptyProtos.Empty,
+      io.grpc.testing.integration.EmptyProtos.Empty> getStartMethod;
+
+  @io.grpc.stub.annotations.RpcMethod(
+      fullMethodName = SERVICE_NAME + '/' + "Start",
