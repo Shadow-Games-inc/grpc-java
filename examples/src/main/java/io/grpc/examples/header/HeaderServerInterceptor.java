407b3c1590a3bdd37a7108399abdedd7a1fffa9e
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-20 00:22:33 +0600

    Update HeaderServerInterceptor.java

--- examples/src/main/java/io/grpc/examples/header/HeaderServerInterceptor.java
+++ examples/src/main/java/io/grpc/examples/header/HeaderServerInterceptor.java
@@ -1,3 +1,6 @@
+/*
+ * Copyright 2015 The gRPC Authors
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -22,3 +25,29 @@ import io.grpc.ServerInterceptor;
 import java.util.logging.Logger;
 
 /**
+ * A interceptor to handle server header.
+ */
+public class HeaderServerInterceptor implements ServerInterceptor {
+
+  private static final Logger logger = Logger.getLogger(HeaderServerInterceptor.class.getName());
+
+  @VisibleForTesting
+  static final Metadata.Key<String> CUSTOM_HEADER_KEY =
+      Metadata.Key.of("custom_server_header_key", Metadata.ASCII_STRING_MARSHALLER);
+
+
+  @Override
+  public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(
+      ServerCall<ReqT, RespT> call,
+      final Metadata requestHeaders,
+      ServerCallHandler<ReqT, RespT> next) {
+    logger.info("header received from client:" + requestHeaders);
+    return next.startCall(new SimpleForwardingServerCall<ReqT, RespT>(call) {
+      @Override
+      public void sendHeaders(Metadata responseHeaders) {
+        responseHeaders.put(CUSTOM_HEADER_KEY, "customRespondValue");
+        super.sendHeaders(responseHeaders);
+      }
+    }, requestHeaders);
+  }
+}
