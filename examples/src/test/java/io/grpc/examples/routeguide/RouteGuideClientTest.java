7b0fbc217a32a0ed85bda5eaf76bc7202e7d0589
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-20 00:23:50 +0600

    Create RouteGuideClientTest.java

---
+++ examples/src/test/java/io/grpc/examples/routeguide/RouteGuideClientTest.java
@@ -0,0 +1,19 @@
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.examples.routeguide;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+import com.google.protobuf.Message;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
+import io.grpc.examples.routeguide.RouteGuideClient.TestHelper;
