f7bf80b036493c5032bb9c923dd40eb313f7b7d9
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-20 00:22:55 +0600

    Create ManualFlowControlServer.java

---
+++ examples/src/main/java/io/grpc/examples/manualflowcontrol/ManualFlowControlServer.java
@@ -0,0 +1,21 @@
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.examples.manualflowcontrol;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.Status;
+import io.grpc.stub.ServerCallStreamObserver;
+import io.grpc.stub.StreamObserver;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+public class ManualFlowControlServer {
+  private static final Logger logger =
