0b499b0893df813a8f7b2f0e8de2b15102fbabc9
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-20 00:23:06 +0600

    Create RouteGuideClient.java

---
+++ examples/src/main/java/io/grpc/examples/routeguide/RouteGuideClient.java
@@ -0,0 +1,21 @@
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.examples.routeguide;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.protobuf.Message;
+import io.grpc.Channel;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
+import io.grpc.examples.routeguide.RouteGuideGrpc.RouteGuideBlockingStub;
+import io.grpc.examples.routeguide.RouteGuideGrpc.RouteGuideStub;
