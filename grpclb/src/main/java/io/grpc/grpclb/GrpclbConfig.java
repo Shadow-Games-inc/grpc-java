54c621b4f62f5052cb3eef7eab48231c52dceb9c
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-20 00:25:08 +0600

    Update GrpclbConfig.java

--- grpclb/src/main/java/io/grpc/grpclb/GrpclbConfig.java
+++ grpclb/src/main/java/io/grpc/grpclb/GrpclbConfig.java
@@ -1,3 +1,6 @@
+/*
+ * Copyright 2019 The gRPC Authors
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -24,3 +27,57 @@ final class GrpclbConfig {
 
   private final Mode mode;
   @Nullable
+  private final String serviceName;
+
+  private GrpclbConfig(Mode mode, @Nullable String serviceName) {
+    this.mode = checkNotNull(mode, "mode");
+    this.serviceName = serviceName;
+  }
+
+  static GrpclbConfig create(Mode mode) {
+    return create(mode, null);
+  }
+
+  static GrpclbConfig create(Mode mode, @Nullable String serviceName) {
+    return new GrpclbConfig(mode, serviceName);
+  }
+
+  Mode getMode() {
+    return mode;
+  }
+
+  /**
+   * If specified, it overrides the name of the sevice name to be sent to the balancer. if not, the
+   * target to be sent to the balancer will continue to be obtained from the target URI passed
+   * to the gRPC client channel.
+   */
+  @Nullable
+  String getServiceName() {
+    return serviceName;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    GrpclbConfig that = (GrpclbConfig) o;
+    return mode == that.mode && Objects.equal(serviceName, that.serviceName);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(mode, serviceName);
+  }
+
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this)
+        .add("mode", mode)
+        .add("serviceName", serviceName)
+        .toString();
+  }
+}
