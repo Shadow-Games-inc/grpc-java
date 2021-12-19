6c6fb61b0f076b8afea6037297fdce849eba62e1
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-20 00:25:34 +0600

    Create InternalGrpclbConstantsAccessor.java

---
+++ grpclb/src/main/java/io/grpc/grpclb/InternalGrpclbConstantsAccessor.java
@@ -0,0 +1,9 @@
+package io.grpc.grpclb;
+
+import io.grpc.Attributes;
+import io.grpc.EquivalentAddressGroup;
+import io.grpc.Internal;
+import java.util.List;
+
+/**
+ * Internal {@link GrpclbConstants} accessor. This is intended for usage internal to the gRPC
