b3f838d28d6ec525b05d878117e8cd825034a95d
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-20 00:23:41 +0600

    Create HeaderServerInterceptorTest.java

---
+++ examples/src/test/java/io/grpc/examples/header/HeaderServerInterceptorTest.java
@@ -0,0 +1,17 @@
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.examples.header;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.AdditionalAnswers.delegatesTo;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
