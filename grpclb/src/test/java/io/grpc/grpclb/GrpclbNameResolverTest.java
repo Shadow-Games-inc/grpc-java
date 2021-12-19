678e93db60cc30ba99809ca038898f5e335b3455
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-20 00:26:57 +0600

    Create GrpclbNameResolverTest.java

---
+++ grpclb/src/test/java/io/grpc/grpclb/GrpclbNameResolverTest.java
@@ -0,0 +1,17 @@
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.grpclb;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.google.common.collect.Iterables;
