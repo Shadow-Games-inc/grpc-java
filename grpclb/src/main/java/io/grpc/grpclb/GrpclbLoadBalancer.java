3bfc73396032567606dbed68506ba49da35e625e
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-20 00:25:13 +0600

    Create GrpclbLoadBalancer.java

---
+++ grpclb/src/main/java/io/grpc/grpclb/GrpclbLoadBalancer.java
@@ -0,0 +1,21 @@
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.grpclb;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Stopwatch;
+import io.grpc.Attributes;
+import io.grpc.ChannelLogger.ChannelLogLevel;
+import io.grpc.Context;
