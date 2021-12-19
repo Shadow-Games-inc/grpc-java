b43f458d22b0e093ba1098aa115e35206573f3c6
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-20 00:23:46 +0600

    Update HelloWorldClientTest.java

--- examples/src/test/java/io/grpc/examples/helloworld/HelloWorldClientTest.java
+++ examples/src/test/java/io/grpc/examples/helloworld/HelloWorldClientTest.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright 2015 The gRPC Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -26,3 +32,73 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatchers;
+
+/**
+ * Unit tests for {@link HelloWorldClient}.
+ * For demonstrating how to write gRPC unit test only.
+ * Not intended to provide a high code coverage or to test every major usecase.
+ *
+ * directExecutor() makes it easier to have deterministic tests.
+ * However, if your implementation uses another thread and uses streaming it is better to use
+ * the default executor, to avoid hitting bug #3084.
+ *
+ * <p>For more unit test examples see {@link io.grpc.examples.routeguide.RouteGuideClientTest} and
+ * {@link io.grpc.examples.routeguide.RouteGuideServerTest}.
+ */
+@RunWith(JUnit4.class)
+public class HelloWorldClientTest {
+  /**
+   * This rule manages automatic graceful shutdown for the registered servers and channels at the
+   * end of test.
+   */
+  @Rule
+  public final GrpcCleanupRule grpcCleanup = new GrpcCleanupRule();
+
+  private final GreeterGrpc.GreeterImplBase serviceImpl =
+      mock(GreeterGrpc.GreeterImplBase.class, delegatesTo(
+          new GreeterGrpc.GreeterImplBase() {
+          // By default the client will receive Status.UNIMPLEMENTED for all RPCs.
+          // You might need to implement necessary behaviors for your test here, like this:
+          //
+          // @Override
+          // public void sayHello(HelloRequest request, StreamObserver<HelloReply> respObserver) {
+          //   respObserver.onNext(HelloReply.getDefaultInstance());
+          //   respObserver.onCompleted();
+          // }
+          }));
+
+  private HelloWorldClient client;
+
+  @Before
+  public void setUp() throws Exception {
+    // Generate a unique in-process server name.
+    String serverName = InProcessServerBuilder.generateName();
+
+    // Create a server, add service, start, and register for automatic graceful shutdown.
+    grpcCleanup.register(InProcessServerBuilder
+        .forName(serverName).directExecutor().addService(serviceImpl).build().start());
+
+    // Create a client channel and register for automatic graceful shutdown.
+    ManagedChannel channel = grpcCleanup.register(
+        InProcessChannelBuilder.forName(serverName).directExecutor().build());
+
+    // Create a HelloWorldClient using the in-process channel;
+    client = new HelloWorldClient(channel);
+  }
+
+  /**
+   * To test the client, call from the client against the fake server, and verify behaviors or state
+   * changes from the server side.
+   */
+  @Test
+  public void greet_messageDeliveredToServer() {
+    ArgumentCaptor<HelloRequest> requestCaptor = ArgumentCaptor.forClass(HelloRequest.class);
+
+    client.greet("test name");
+
+    verify(serviceImpl)
+        .sayHello(requestCaptor.capture(), ArgumentMatchers.<StreamObserver<HelloReply>>any());
+    assertEquals("test name", requestCaptor.getValue().getName());
+  }
+}
