085c9fccf648158ef92f04f52f39a7159ffac3fc
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-20 00:22:40 +0600

    Create HedgingHelloWorldServer.java

---
+++ examples/src/main/java/io/grpc/examples/hedging/HedgingHelloWorldServer.java
@@ -0,0 +1,20 @@
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.examples.hedging;
+
+import io.grpc.Metadata;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.ServerCall;
+import io.grpc.ServerCall.Listener;
+import io.grpc.ServerCallHandler;
+import io.grpc.ServerInterceptor;
+import io.grpc.examples.helloworld.GreeterGrpc;
+import io.grpc.examples.helloworld.HelloReply;
+import io.grpc.examples.helloworld.HelloRequest;
