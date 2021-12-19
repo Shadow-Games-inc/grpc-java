837a55b5a228f9f59caef19802f180a666b4f1f8
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-20 00:27:35 +0600

    Create XdsUpdateHealthServiceGrpc.java

---
+++ interop-testing/src/generated/main/grpc/io/grpc/testing/integration/XdsUpdateHealthServiceGrpc.java
@@ -0,0 +1,16 @@
+ */
+@javax.annotation.Generated(
+    value = "by gRPC proto compiler",
+    comments = "Source: grpc/testing/test.proto")
+@io.grpc.stub.annotations.GrpcGenerated
+public final class XdsUpdateHealthServiceGrpc {
+
+  private XdsUpdateHealthServiceGrpc() {}
+
+  public static final String SERVICE_NAME = "grpc.testing.XdsUpdateHealthService";
+
+  // Static method descriptors that strictly reflect the proto.
+  private static volatile io.grpc.MethodDescriptor<io.grpc.testing.integration.EmptyProtos.Empty,
+      io.grpc.testing.integration.EmptyProtos.Empty> getSetServingMethod;
+
+  @io.grpc.stub.annotations.RpcMethod(
