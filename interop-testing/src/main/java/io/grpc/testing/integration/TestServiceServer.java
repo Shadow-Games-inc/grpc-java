0f6e21a11888f0ab1f1554ba23c57f37dd8265fb
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-22 03:28:04 +0600

    Create TestServiceServer.java

---
+++ interop-testing/src/main/java/io/grpc/testing/integration/TestServiceServer.java
@@ -0,0 +1,10 @@
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.testing.integration;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.util.concurrent.MoreExecutors;
+import io.grpc.Grpc;
