17893ae821dc50b717379b1ee4c5e544432ac77d
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-21 16:28:02 +0600

    Update TestCases.java

--- interop-testing/src/main/java/io/grpc/testing/integration/TestCases.java
+++ interop-testing/src/main/java/io/grpc/testing/integration/TestCases.java
@@ -33,3 +33,50 @@ public enum TestCases {
   CLIENT_COMPRESSED_STREAMING("client per-message compression on stream"),
   CLIENT_COMPRESSED_STREAMING_NOPROBE(
       "client per-message compression on stream (skip initial feature-probing request)"),
+  SERVER_STREAMING("single request with response streaming"),
+  SERVER_COMPRESSED_STREAMING("server per-message compression on stream"),
+  PING_PONG("full-duplex ping-pong streaming"),
+  EMPTY_STREAM("A stream that has zero-messages in both directions"),
+  COMPUTE_ENGINE_CREDS("large_unary with service_account auth"),
+  COMPUTE_ENGINE_CHANNEL_CREDENTIALS("large unary with compute engine channel builder"),
+  SERVICE_ACCOUNT_CREDS("large_unary with compute engine auth"),
+  JWT_TOKEN_CREDS("JWT-based auth"),
+  OAUTH2_AUTH_TOKEN("raw oauth2 access token auth"),
+  PER_RPC_CREDS("per rpc raw oauth2 access token auth"),
+  GOOGLE_DEFAULT_CREDENTIALS(
+      "google default credentials, i.e. GoogleManagedChannel based auth"),
+  CUSTOM_METADATA("unary and full duplex calls with metadata"),
+  STATUS_CODE_AND_MESSAGE("request error code and message"),
+  SPECIAL_STATUS_MESSAGE("special characters in status message"),
+  UNIMPLEMENTED_METHOD("call an unimplemented RPC method"),
+  UNIMPLEMENTED_SERVICE("call an unimplemented RPC service"),
+  CANCEL_AFTER_BEGIN("cancel stream after starting it"),
+  CANCEL_AFTER_FIRST_RESPONSE("cancel on first response"),
+  TIMEOUT_ON_SLEEPING_SERVER("timeout before receiving a response"),
+  VERY_LARGE_REQUEST("very large request"),
+  PICK_FIRST_UNARY("all requests are sent to one server despite multiple servers are resolved"),
+  RPC_SOAK("sends 'soak_iterations' large_unary rpcs in a loop, each on the same channel"),
+  CHANNEL_SOAK("sends 'soak_iterations' large_unary rpcs in a loop, each on a new channel");
+
+  private final String description;
+
+  TestCases(String description) {
+    this.description = description;
+  }
+
+  /**
+   * Returns a description of the test case.
+   */
+  public String description() {
+    return description;
+  }
+
+  /**
+   * Returns the {@link TestCases} matching the string {@code s}. The
+   * matching is done case insensitive.
+   */
+  public static TestCases fromString(String s) {
+    Preconditions.checkNotNull(s, "s");
+    return TestCases.valueOf(s.toUpperCase());
+  }
+}
