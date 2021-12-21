be11776c5b330a7dff574b051e95311ea991d8ea
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-21 16:17:21 +0600

    Update ReconnectTestClient.java

--- interop-testing/src/main/java/io/grpc/testing/integration/ReconnectTestClient.java
+++ interop-testing/src/main/java/io/grpc/testing/integration/ReconnectTestClient.java
@@ -1,3 +1,5 @@
+/*
+ * Copyright 2015 The gRPC Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,3 +15,18 @@
  */
 
 package io.grpc.testing.integration;
+
+import static org.junit.Assert.assertTrue;
+
+import io.grpc.ManagedChannel;
+import io.grpc.StatusRuntimeException;
+import io.grpc.netty.NegotiationType;
+import io.grpc.netty.NettyChannelBuilder;
+import io.grpc.okhttp.OkHttpChannelBuilder;
+import io.grpc.testing.integration.EmptyProtos.Empty;
+import io.grpc.testing.integration.Messages.ReconnectInfo;
+
+/**
+ * Verifies the client is reconnecting the server with correct backoffs
+ *
+ * <p>See the <a href="https://github.com/grpc/grpc/blob/master/doc/connection-backoff-interop-test-description.md">Test Spec</a>.
