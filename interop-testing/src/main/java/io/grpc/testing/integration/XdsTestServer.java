99ff9e9a61fca5e0fd09e1b12074cf09f981da6d
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-22 03:28:17 +0600

    Create XdsTestServer.java

---
+++ interop-testing/src/main/java/io/grpc/testing/integration/XdsTestServer.java
@@ -0,0 +1,24 @@
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.testing.integration;
+
+import com.google.common.base.Splitter;
+import com.google.common.collect.Iterables;
+import io.grpc.ForwardingServerCall.SimpleForwardingServerCall;
+import io.grpc.InsecureServerCredentials;
+import io.grpc.Metadata;
+import io.grpc.Server;
+import io.grpc.ServerCall;
+import io.grpc.ServerCallHandler;
+import io.grpc.ServerInterceptor;
+import io.grpc.ServerInterceptors;
+import io.grpc.Status;
+import io.grpc.health.v1.HealthCheckResponse.ServingStatus;
+import io.grpc.netty.NettyServerBuilder;
+import io.grpc.protobuf.services.HealthStatusManager;
+import io.grpc.protobuf.services.ProtoReflectionService;
+import io.grpc.services.AdminInterface;
+import io.grpc.stub.StreamObserver;
