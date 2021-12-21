88270dae6ea8e2678915210055b336b9126f0a8e
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-21 16:20:53 +0600

    Update StressTestClient.java

--- interop-testing/src/main/java/io/grpc/testing/integration/StressTestClient.java
+++ interop-testing/src/main/java/io/grpc/testing/integration/StressTestClient.java
@@ -349,3 +349,9 @@ public class StressTestClient {
         throw new RuntimeException(ex);
       }
     }
+    return NettyChannelBuilder.forAddress(address)
+        .negotiationType(useTls ? NegotiationType.TLS : NegotiationType.PLAINTEXT)
+        .sslContext(sslContext)
+        .build();
+  }
+
