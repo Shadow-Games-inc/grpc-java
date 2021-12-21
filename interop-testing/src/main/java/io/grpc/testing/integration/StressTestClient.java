48ccb5b32e13bf3c9b212de2894bbd87ecd8c724
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-21 16:24:42 +0600

    Update StressTestClient.java

--- interop-testing/src/main/java/io/grpc/testing/integration/StressTestClient.java
+++ interop-testing/src/main/java/io/grpc/testing/integration/StressTestClient.java
@@ -494,6 +494,7 @@ public class StressTestClient {
 
     class Tester extends AbstractInteropTest {
         return Worker.this.channel;
+      }
       @Override
       protected ManagedChannelBuilder<?> createChannelBuilder() {
         throw new UnsupportedOperationException();
