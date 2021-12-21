349a2db78bb90f5bfa4f697f35fbcbcd466004f5
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-22 03:28:09 +0600

    Create Util.java

---
+++ interop-testing/src/main/java/io/grpc/testing/integration/Util.java
@@ -0,0 +1,17 @@
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.testing.integration;
+
+import com.google.protobuf.MessageLite;
+import io.grpc.Metadata;
+import io.grpc.protobuf.lite.ProtoLiteUtils;
+import java.util.List;
+import org.junit.Assert;
+
+/**
+ * Utility methods to support integration testing.
