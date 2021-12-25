8ed1f7bbc09d441d7c7279cda0e57d19a65f24cd
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-25 12:40:58 +0600

    Update AltsHandshakerTest.java

--- interop-testing/src/test/java/io/grpc/testing/integration/AltsHandshakerTest.java
+++ interop-testing/src/test/java/io/grpc/testing/integration/AltsHandshakerTest.java
@@ -11,3 +11,15 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.testing.integration;
+
+import static org.junit.Assert.assertEquals;
+
+import com.google.protobuf.ByteString;
+import io.grpc.ChannelCredentials;
+import io.grpc.Grpc;
+import io.grpc.ManagedChannel;
+import io.grpc.Server;
