01fbb6ccf1ab1afcbe33e47f95f742926ff5b2c7
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-26 00:27:44 +0600

    Update CascadingTest.java

--- interop-testing/src/test/java/io/grpc/testing/integration/CascadingTest.java
+++ interop-testing/src/test/java/io/grpc/testing/integration/CascadingTest.java
@@ -255,6 +255,9 @@ public class CascadingTest {
                       req = req.toBuilder().setResponseSize(req.getResponseSize() - 1).build();
                               public void onNext(Messages.SimpleResponse value) {
                               public void onError(Throwable t) {
+                                Status status = Status.fromThrowable(t);
+                                if (status.getCode() == Status.Code.CANCELLED) {
+                                  observedCancellations.countDown();
                                 }
                                 // Propagate closure upwards.
                                 try {
