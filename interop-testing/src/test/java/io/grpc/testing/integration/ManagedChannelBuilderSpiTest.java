8f00f29cb8434440ed8a23aa65180e86c5bbf692
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-29 22:51:53 +0600

    Update ManagedChannelBuilderSpiTest.java

--- interop-testing/src/test/java/io/grpc/testing/integration/ManagedChannelBuilderSpiTest.java
+++ interop-testing/src/test/java/io/grpc/testing/integration/ManagedChannelBuilderSpiTest.java
@@ -7,3 +7,18 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.testing.integration;
+
+import io.grpc.ManagedChannelBuilder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Tests for integration between {@link ManagedChannelBuilder} and providers. */
