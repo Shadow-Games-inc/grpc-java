f81ad7ce336fb7c633546fe6c1dc417fc2f3b311
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-31 00:19:29 +0600

    Update StressTestClientTest.java

--- interop-testing/src/test/java/io/grpc/testing/integration/StressTestClientTest.java
+++ interop-testing/src/test/java/io/grpc/testing/integration/StressTestClientTest.java
@@ -135,3 +135,6 @@ public class StressTestClientTest {
 
     MetricsServiceGrpc.MetricsServiceBlockingStub stub = MetricsServiceGrpc.newBlockingStub(ch);
 
+    // Wait until gauges have been exported
+    Set<String> gaugeNames = newHashSet("/stress_test/server_0/channel_0/stub_0/qps",
+        "/stress_test/server_0/channel_0/stub_1/qps");
