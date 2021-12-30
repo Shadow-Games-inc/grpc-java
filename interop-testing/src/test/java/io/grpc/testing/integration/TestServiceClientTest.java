7fd3c8cea8a912932f46196774e89963748a9068
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-31 00:23:28 +0600

    Update TestServiceClientTest.java

--- interop-testing/src/test/java/io/grpc/testing/integration/TestServiceClientTest.java
+++ interop-testing/src/test/java/io/grpc/testing/integration/TestServiceClientTest.java
@@ -18,3 +18,6 @@ package io.grpc.testing.integration;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Unit tests for {@link TestServiceClient}. */
