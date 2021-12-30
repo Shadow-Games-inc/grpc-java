4e9b6edc47f52391a4643dcd451ac34e82e47a06
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-31 00:25:03 +0600

    Update TrafficControlProxy.java

--- interop-testing/src/test/java/io/grpc/testing/integration/TrafficControlProxy.java
+++ interop-testing/src/test/java/io/grpc/testing/integration/TrafficControlProxy.java
@@ -22,3 +22,6 @@ import io.netty.util.concurrent.DefaultThreadFactory;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
