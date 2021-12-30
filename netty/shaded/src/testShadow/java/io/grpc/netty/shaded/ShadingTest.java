5a74d8a1299105dedc3c7328a3f84634846a098a
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-31 01:04:36 +0600

    Update ShadingTest.java

--- netty/shaded/src/testShadow/java/io/grpc/netty/shaded/ShadingTest.java
+++ netty/shaded/src/testShadow/java/io/grpc/netty/shaded/ShadingTest.java
@@ -34,3 +34,6 @@ import io.grpc.netty.shaded.io.grpc.netty.NettySslContextChannelCredentials;
 import io.grpc.netty.shaded.io.netty.handler.ssl.SslContextBuilder;
 import io.grpc.netty.shaded.io.netty.handler.ssl.SslProvider;
 import io.grpc.stub.StreamObserver;
+import io.grpc.testing.protobuf.SimpleRequest;
+import io.grpc.testing.protobuf.SimpleResponse;
+import io.grpc.testing.protobuf.SimpleServiceGrpc;
