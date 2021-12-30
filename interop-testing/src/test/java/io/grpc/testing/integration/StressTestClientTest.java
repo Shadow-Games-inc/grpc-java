361c99d6794d873dd7e90bfdaa3061076ec381cf
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-31 00:16:00 +0600

    Update StressTestClientTest.java

--- interop-testing/src/test/java/io/grpc/testing/integration/StressTestClientTest.java
+++ interop-testing/src/test/java/io/grpc/testing/integration/StressTestClientTest.java
@@ -62,3 +62,6 @@ public class StressTestClientTest {
   public void defaults() {
     StressTestClient client = new StressTestClient();
     assertEquals(singletonList(new InetSocketAddress("localhost", 8080)), client.addresses());
+    client.parseArgs(new String[] {
+        "--server_addresses=localhost:8080,localhost:8081,localhost:8082",
+        "--test_cases=empty_unary:20,large_unary:50,server_streaming:30",
