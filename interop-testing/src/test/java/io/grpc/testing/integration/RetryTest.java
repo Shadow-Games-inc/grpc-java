5f63d220a16e2bde2f9bdd62d95bc80d2593ee6a
raihan <54474184+ryihan@users.noreply.github.com>
2021-12-31 00:07:51 +0600

    Update RetryTest.java

--- interop-testing/src/test/java/io/grpc/testing/integration/RetryTest.java
+++ interop-testing/src/test/java/io/grpc/testing/integration/RetryTest.java
@@ -353,3 +353,6 @@ public class RetryTest {
     assertInboundWireSizeRecorded(1);
     serverCall.close(Status.OK, new Metadata());
     assertRpcStatusRecorded(Status.Code.OK, 2000, 2);
+    assertRetryStatsRecorded(1, 0, 10_000);
+  }
+
