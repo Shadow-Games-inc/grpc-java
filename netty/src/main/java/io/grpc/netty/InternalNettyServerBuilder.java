3049e2541c928fd756ba8af558b473a6583e15c4
raihan <54474184+ryihan@users.noreply.github.com>
2022-08-16 12:40:26 +0600

    Update InternalNettyServerBuilder.java

--- netty/src/main/java/io/grpc/netty/InternalNettyServerBuilder.java
+++ netty/src/main/java/io/grpc/netty/InternalNettyServerBuilder.java
@@ -1,3 +1,7 @@
   public static void setStatsRecordRealTimeMetrics(NettyServerBuilder builder, boolean value) {
     builder.setStatsRecordRealTimeMetrics(value);
   }
+
+  public static void setTracingEnabled(NettyServerBuilder builder, boolean value) {
+    builder.setTracingEnabled(value);
+  }
