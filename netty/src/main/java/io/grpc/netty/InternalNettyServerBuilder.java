f6cbda62704376d6f55fb52068fba3dbe6529eec
raihan <54474184+ryihan@users.noreply.github.com>
2022-08-16 12:40:37 +0600

    Update InternalNettyServerBuilder.java

--- netty/src/main/java/io/grpc/netty/InternalNettyServerBuilder.java
+++ netty/src/main/java/io/grpc/netty/InternalNettyServerBuilder.java
@@ -5,3 +5,11 @@
   public static void setTracingEnabled(NettyServerBuilder builder, boolean value) {
     builder.setTracingEnabled(value);
   }
+  public static void useNioTransport(NettyServerBuilder builder) {
+    builder.channelType(NioServerSocketChannel.class);
+    builder
+        .bossEventLoopGroupPool(SharedResourcePool.forResource(Utils.NIO_BOSS_EVENT_LOOP_GROUP));
+    builder
+        .workerEventLoopGroupPool(
+            SharedResourcePool.forResource(Utils.NIO_WORKER_EVENT_LOOP_GROUP));
+  }
